3ee98f97f41874bcba6db8e7b977cd669c3ba1a8
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2026-09-14 13:05:51 +0300

    Lecture update.

--- lecture1/firstapp.py
+++ lecture1/firstapp.py
@@ -1,3 +1,11 @@
 print("Hello world!")
+
+
+
+
+
+
+
+
 print("You can also run your code with Ctrl + Shift + F10 in Windows!")
 print("In MacOS: CMD + Shift + R")
---
+++ lecture2/lecture2_1.py
@@ -0,0 +1,19 @@
+print("Testing!")
+
+# remember to also print a variable if you want to see
+# the result variables do nothing in code by themselves,
+# unless they are used somehow
+age = 35
+print(age)
+
+# let's try another variable
+salary = 3200
+print(salary)
+
+# text values (string), use quotation marks
+name = "Test Person"
+print(name)
+
+# decimals values (float)
+tax = 1.24
+print(tax)
