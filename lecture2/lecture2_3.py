6df47592fab29cbdc6ead9e093cdbbb96f3606a6
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2026-09-14 14:12:06 +0300

    Lecture update.

--- lecture2/lecture2_3.py
+++ lecture2/lecture2_3.py
@@ -8,4 +8,12 @@ debt = 400
 
 # calculate all money together based on variables
 total_money = salary + savings - debt
-print(total_money)
+
+# let's create a name variable too
+name = "Test Person"
+
+# print out the result with f-string
+# f-string takes a little bit time to get used to
+# but is by far the easiest text/variable -combination
+# tool in the long run
+print(f"Name: {name}, total money: {total_money} €")
