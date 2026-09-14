dcd185e1d02a90c24a86dd224c99452a59972338
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2026-09-14 14:22:51 +0300

    Lecture update.

---
+++ lecture2/lecture2_4.py
@@ -0,0 +1,14 @@
+# adding new lines within text
+# if you literally need a backslash, try \\
+print("Some text here!\nThis goes to another line!\n\n")
+
+# empty print() also adds a single \n by default
+# NOTE: sometimes \n is not enough, you might need to add
+# \r as well (carriage return)
+print("Something else...")
+print()
+
+# tabs can be used to align pieces of text horizontally
+print("Total:\t\t350 €")
+print("Tax:\t\t120 €")
+print("Postage:\t20 €")
