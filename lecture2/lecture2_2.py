07004f837884836a2033554d177862bca62fc4fa
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2026-09-14 13:38:46 +0300

    Lecture update.

---
+++ lecture2/lecture2_2.py
@@ -0,0 +1,33 @@
+# summing up values
+total = 176 + 287
+print(total)
+
+# substraction example
+difference = 1856 - 917
+print(difference)
+
+# we typically use variables in calculations
+# instead of just numeric values
+salary = 2700
+
+# basically => change salary to be a new value
+# in this case: NEW SALARY value => OLD SALARY * 1.2
+# NOTE! this code adds +20% to old salary
+# easiest way => multiply by 1.2
+salary = salary * 1.2
+print(salary)
+
+# typical division
+wind_speed = 25 / 3.6
+print(wind_speed)
+
+# you can use // to remove the decimals
+apples = 25 // 4
+print(apples)
+
+# remainder (modulus) is the opposite of //
+# ==> %
+# the reasoning: 4 * 6 = 24
+# 25 - 24 => 1
+leftover = 25 % 4
+print(leftover)
