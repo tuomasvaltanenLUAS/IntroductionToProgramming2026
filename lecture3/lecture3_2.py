58af18d7a7f9a163f6fa709ec33f5346cfe42677
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2026-09-22 13:22:59 +0300

    Lecture update.

---
+++ lecture3/lecture3_2.py
@@ -0,0 +1,7 @@
+# this is a specific import
+# from datetime-module => import one feature called "date" ONLY
+from datetime import date
+
+# get current timestamp from Python
+today = date.today()
+print(today)
